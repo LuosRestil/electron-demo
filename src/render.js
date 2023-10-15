4a5bc21b1285a5c310fa0ab55b8cf43d20992ae7
Luos Restil <luosrestil@Luoss-MBP.hitronhub.home>
2023-10-15 11:13:55 -0400

    save video to file

--- src/render.js
+++ src/render.js
@@ -1,33 +1,38 @@
-const { ipcRenderer } = require('electron');
-const { Menu } = require('@electron/remote');
+const { ipcRenderer } = require("electron");
+const { Menu } = require("@electron/remote");
+const fs = require('fs');
 
 const desktopCapturer = {
-  getSources: (opts) => ipcRenderer.invoke('DESKTOP_CAPTURER_GET_SOURCES', opts)
-}
+  getSources: (opts) =>
+    ipcRenderer.invoke("DESKTOP_CAPTURER_GET_SOURCES", opts),
+};
 
-const video = document.querySelector('video');
-const startBtn = document.getElementById('startBtn');
-const endBtn = document.getElementById('endBtn');
-const videoSelectBtn = document.getElementById('videoSelectBtn');
+const video = document.querySelector("video");
+const startBtn = document.getElementById("startBtn");
+const endBtn = document.getElementById("endBtn");
+const videoSelectBtn = document.getElementById("videoSelectBtn");
 
 videoSelectBtn.onclick = getVideoSources;
 
 async function getVideoSources() {
   const inputSources = await desktopCapturer.getSources({
-    types: ['window', 'screen']
+    types: ["window", "screen"],
   });
 
   const videoOptionsMenu = Menu.buildFromTemplate(
-    inputSources.map(source => {
+    inputSources.map((source) => {
       return {
         label: source.name,
         click: () => selectSource(source),
-      }
+      };
     })
   );
 
   videoOptionsMenu.popup();
 }
+
+let mediaRecorder;
+const recordedChunks = [];
 
 async function selectSource(source) {
   videoSelectBtn.innerText = source.name;
@@ -36,15 +41,45 @@ async function selectSource(source) {
     audio: false,
     video: {
       mandatory: {
-        chromeMediaSource: 'desktop',
-        chromeMediaSourceId: source.id
-      }
-    }
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id,
+      },
+    },
   };
 
   // create a stream of video output from the source window/screen
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
 
+  // preview the source in video element
   video.srcObject = stream;
   video.play();
+
+  // create the media recorder
+  const options = { mimeType: "video/webm; codecs=vp9" };
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  // event handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+}
+
+function handleDataAvailable(data) {
+  recordedChunks.push(e.data);
+}
+
+async function handleStop(evt) {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm; codecs=vp9"
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const {filepath} = await dialog.showSaveDialog({
+    buttonLabel: 'Save Video',
+    defaultPath: `vid-${Date.now()}.webm`
+  });
+
+  fs.writeFile(filepath, buffer, () => {
+    console.log('Video saved successfully!');
+  });
 }
